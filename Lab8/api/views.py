1e6068a2672f7f0836645542f3f2952588aa127b
Abylkairkhan <98662517+Abylkairkhan@users.noreply.github.com>
2023-04-04 22:11:07 +0600

    Add files via upload

---
+++ Lab8/api/views.py
@@ -0,0 +1,31 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
+
+from api.models import Category, Product
+
+
+def product_list(request):
+    products = Product.objects.all()
+    data = {'products': list(products.values())}
+    return JsonResponse(data)
+
+def product_detail(request, id):
+    product = get_object_or_404(Product, id=id)
+    data = {'product': product.__dict__}
+    return JsonResponse(data)
+
+def category_list(request):
+    categories = Category.objects.all()
+    data = {'categories': list(categories.values())}
+    return JsonResponse(data)
+
+def category_detail(request, id):
+    category = get_object_or_404(Category, id=id)
+    data = {'category': category.__dict__}
+    return JsonResponse(data)
+
+def category_products(request, id):
+    category = get_object_or_404(Category, id=id)
+    products = category.product_set.all()
+    data = {'products': list(products.values())}
+    return JsonResponse(data)
